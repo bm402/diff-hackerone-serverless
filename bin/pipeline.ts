d9647f6488913eefe7865a1286efe72b33f3bef8
bncrypted <bncrypted@gmail.com>
2021-01-06 12:22:23 +0000

    added cdk scripts

---
+++ bin/pipeline.ts
@@ -0,0 +1,12 @@
+import { App } from '@aws-cdk/core';
+import { LambdaStack } from '../lib/lambda-stack';
+import { PipelineStack } from '../lib/pipeline-stack';
+
+const app = new App();
+
+const lambdaStack = new LambdaStack(app, 'LambdaStack');
+new PipelineStack(app, 'PipelineDeployingLambdaStack', {
+  lambdaCode: lambdaStack.lambdaCode
+});
+
+app.synth();
---
+++ lib/pipeline-stack.ts
@@ -0,0 +1,117 @@
+import * as codebuild from '@aws-cdk/aws-codebuild';
+import * as codepipeline from '@aws-cdk/aws-codepipeline';
+import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
+import * as lambda from '@aws-cdk/aws-lambda';
+import { App, Stack, StackProps, SecretValue } from '@aws-cdk/core';
+
+export interface PipelineStackProps extends StackProps {
+  readonly lambdaCode: lambda.CfnParametersCode;
+}
+
+export class PipelineStack extends Stack {
+  constructor(app: App, id: string, props: PipelineStackProps) {
+    super(app, id, props);
+
+    const cdkBuild = new codebuild.PipelineProject(this, 'CdkBuild', {
+      buildSpec: codebuild.BuildSpec.fromObject({
+        version: '0.2',
+        phases: {
+          install: {
+            commands: 'npm install',
+          },
+          build: {
+            commands: [
+              'npm run build',
+              'npm run cdk synth -- -o dist'
+            ],
+          },
+        },
+        artifacts: {
+          'base-directory': 'dist',
+          files: [
+            'LambdaStack.template.json',
+          ],
+        },
+      }),
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_4_0,
+      },
+    });
+
+    const lambdaBuild = new codebuild.PipelineProject(this, 'LambdaBuild', {
+      buildSpec: codebuild.BuildSpec.fromObject({
+        version: '0.2',
+        phases: {
+          install: {
+            commands: 'cd app',
+          },
+          build: {
+            commands: 'GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o main *.go',
+          },
+        },
+        artifacts: {
+          'base-directory': 'app',
+          files: [
+            '*.go'
+          ],
+        },
+      }),
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_4_0,
+      },
+    });
+
+    const sourceOutput = new codepipeline.Artifact();
+    const cdkBuildOutput = new codepipeline.Artifact('CdkBuildOutput');
+    const lambdaBuildOutput = new codepipeline.Artifact('LambdaBuildOutput');
+    new codepipeline.Pipeline(this, 'Pipeline', {
+      stages: [
+        {
+          stageName: 'GitHub_Source',
+          actions: [
+            new codepipeline_actions.GitHubSourceAction({
+                actionName: 'GitHub_Source',
+                owner: 'bncrypted',
+                repo: 'diff-hackerone-serverless',
+                oauthToken: SecretValue.ssmSecure('GITHUB_ACCESS_TOKEN', '1'),
+                output: sourceOutput,
+                branch: 'aws',
+              })
+          ],
+        },
+        {
+          stageName: 'Build',
+          actions: [
+            new codepipeline_actions.CodeBuildAction({
+              actionName: 'Lambda_Build',
+              project: lambdaBuild,
+              input: sourceOutput,
+              outputs: [lambdaBuildOutput],
+            }),
+            new codepipeline_actions.CodeBuildAction({
+              actionName: 'CDK_Build',
+              project: cdkBuild,
+              input: sourceOutput,
+              outputs: [cdkBuildOutput],
+            }),
+          ],
+        },
+        {
+          stageName: 'Deploy',
+          actions: [
+            new codepipeline_actions.CloudFormationCreateUpdateStackAction({
+              actionName: 'Lambda_Deploy',
+              templatePath: cdkBuildOutput.atPath('LambdaStack.template.json'),
+              stackName: 'DiffHackeroneServerlessLambdaDeploymentStack',
+              adminPermissions: true,
+              parameterOverrides: {
+                ...props.lambdaCode.assign(lambdaBuildOutput.s3Location),
+              },
+              extraInputs: [lambdaBuildOutput],
+            }),
+          ],
+        },
+      ],
+    });
+  }
+}
